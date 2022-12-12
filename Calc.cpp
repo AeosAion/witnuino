b48dc8cf86bee224a662e55f8195b4ad6b57f9f4
chris <ch.feess@posteo.net>
2022-12-12 15:21:46 +0100

    Fixed collision error

--- Calc.cpp
+++ Calc.cpp
@@ -8,7 +8,7 @@ Calc::Calc(){
 bool Calc::collision(int x1, int y1, int x2, int y2, int radius){
   long a = abs(x1 - x2);
   long b = abs(y1 - y2);
-  long c = round(sqrt(pow(a, 2) + (b, 2)));
+  long c = round(sqrt(pow(a, 2) + pow(b, 2)));
   if(c <= radius){
     return true;
   }
@@ -19,6 +19,6 @@ bool Calc::collision(int x1, int y1, int x2, int y2, int radius){
 int Calc::distance(int _x1, int _y1, int _x2, int _y2){
   long a = abs(_x1 - _x2);
   long b = abs(_y1 - _y2);
-  long c = sqrt(pow(a, 2) + pow(b, 2));
+  long c = round(sqrt(pow(a, 2) + pow(b, 2)));
   return c;
 }
